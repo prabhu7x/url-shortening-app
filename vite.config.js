472ee05fffba2eb916ebd0a1fb505e0327d5ace4
prabhu7x <prabhu7x@gmail.com>
2025-03-09 11:35:04 +0530

    Readme, title and fevicon update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/multi-step-form-main/',
+  base: '/url-shortening-app/',
   plugins: [react()],
 });
 
